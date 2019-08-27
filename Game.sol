6ea4421db89d3b2ac523bac0fdcddcec768523ff
prospect-man <46277489+prospect-man@users.noreply.github.com>
2019-08-27 14:05:20 +0100

    Update Game.sol

--- Game.sol
+++ Game.sol
@@ -1,3 +1,4 @@
+
 //Smart contract to create game inside blockchain
 
 pragma solidity >=0.4.22 <0.6.0;
@@ -6,11 +7,18 @@ pragma solidity >=0.4.22 <0.6.0;
 contract Game {
 
     //Game ID a unique id for game addressof
-    string public GameId = "firstgame"+"{address of game creator}"
-    //maximum number of item
-    uint256 public item = 100000000
+    string public GameId = address + "Game name" 
     
-    function GetGamerItems () public {
+    //maximum number of item
+    uint256 public item1 = 100000000
+    uint256 public item2 = 100000000
+    uint256 public item3 = 100000000
+    uint256 public item4 = 100000000
+    uint256 public item5 = 100000000
+    uint256 public item6 = 100000000
+    uint256 public item7 = 100000000
+    
+    function GetGamerItems (address gamer, address GameId, string password) public {
      // return the number of everytime in-game for a given gamer(address);
     }
 }
