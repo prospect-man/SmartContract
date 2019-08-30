0f5ec832788c5db44ab36b8f4a0e4b5613f2cb58
prospect-man <46277489+prospect-man@users.noreply.github.com>
2019-08-30 22:53:31 +0100

    Update Gamer.sol

--- Gamer.sol
+++ Gamer.sol
@@ -1,1 +1,35 @@
 
+//Smart contract to create game inside blockchain
+pragma solidity ^0.5.11;
+
+contract Game{
+
+    //Game ID a unique id for game
+    address public manager;
+    address[] public games;
+    address[] public gamers;
+    
+    function Owner() public {
+        manager = msg.sender;
+    }
+    
+        function addGames() public payable{
+        require(msg.value > .00001 ether);
+        games.push(msg.sender);
+    }
+    
+    function GamesList() public payable returns (address[] memory){
+        require(msg.value > .00001 ether);
+        return games;
+    }
+    
+    function addGamer() public payable{
+        require(msg.value > .00001 ether);
+        gamers.push(msg.sender);
+    }
+    
+    function GamersList() public payable returns (address[] memory){
+        require(msg.value > .00001 ether);
+        return gamers;
+    }
+}
