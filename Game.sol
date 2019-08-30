72fb777b2d89fffb9c36b1ad6bee5b267afd7a0e
prospect-man <46277489+prospect-man@users.noreply.github.com>
2019-08-30 22:03:04 +0100

    Update Game.sol

--- Game.sol
+++ Game.sol
@@ -1,6 +1,7 @@
 
 //Atheios blockchain
-//Smart contract to integrate game inside blockchain and save the gamer levels and state
+//Our vision is developing video game / blockchain based projects on the Atheios network.
+//Smart contract to integrate game inside blockchain and Storing and Backing Up Your Games.
 
 pragma solidity ^0.5.11;
 
@@ -21,37 +22,4 @@ contract Game{
     function GamersList() public view returns (address[] memory){
         return gamers;
     }
-    
-//the name of item     
-    string[] public items;
-    
-//the name of item     
-    uint8[] public level;
-    
-//maximum number of item
-    uint[] public itemsBalance
-    
-    mapping (address => uint) itemsBalance;
-    
-// return the balance of items in-game for a given gamer(address);
-    function GetItemsBalance (address gamer, string password) external constant returns(uint) {
-        return itemsBalance[gamer]
-    }
-    
-    //Buy item
-    function buyGameItems()
-    
-    //Sell item
-    function sellGameITems()
-    
-    //Transfer item to the game owner address
-    function transferItem(address _from, address _to, uint amount){
-        //
-    }
-    
-    //Listen to gamer while he plays
-    function updateItemsbamance (address gamer, string password, action) return GetItemsBalance(){
-        transferItem () //call
-    }
-    
 }
